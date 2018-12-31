702c977e8ddb93f07505e8dce81d0572128ea6db
Calin Novo <46006265+CREATORGAME19@users.noreply.github.com>
2018-12-31 15:16:50 +0000

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 import os
 import urllib.request
+f = open("MARKER.py","w")
+f.close()
 os.remove("MARKER.py")
 data0 = urllib.request.urlretrieve("https://raw.githubusercontent.com/CREATORGAME19/MARKER/master/MARKER.py","MARKER.py")
 print(os.system('MARKER.py'))
